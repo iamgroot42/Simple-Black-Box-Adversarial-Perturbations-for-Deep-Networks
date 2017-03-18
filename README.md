768596420b0c8082680cabd29b0673fd003c62e3
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-03-18 16:48:50 +0530

    Working

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Simple-Black-Box-Adversarial-Perturbations-for-Deep-Networks
-Implementation of 'Simple Black-Box Adversarial Perturbations for Deep Networks' https://openreview.net/pdf?id=SJCscQcge in Keras and Tensorflow
+Implementation of 'Simple Black-Box Adversarial Perturbations for Deep Networks' https://openreview.net/pdf?id=SJCscQcge in Keras
+
+
+### Running it
+
+* `python main.py <KERAS_MODEL> <IMAGE_in_NUMPY>` : currently works for cifar images only. 
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import keras
 from keras.models import load_model
+import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
 
-def score(model, imgs, c):
-	return model.predict_proba(imgs)[:,c]
+def show_image(img):
+	imgcpy = np.copy(img)
+	imgcpy = np.swapaxes(imgcpy,0,2)
+	imgcpy = np.swapaxes(imgcpy,0,1)
+	plt.imshow(imgcpy)
+	plt.show()
 
 
 def cyclic(value, lb, ub):
@@ -19,8 +24,8 @@ def cyclic(value, lb, ub):
 
 def top_k_predicitons(model, X, k):
 	proba = model.predict_proba(X)
-	labels = np.argsort(x, axis=1)
-	selected_labels = labels[:,k]
+	labels = np.argsort(X, axis=1)
+	selected_labels = (labels[:,:k])[0]
 	return selected_labels
 
 
@@ -37,24 +42,25 @@ def randadv(model, img, label, p, U):
 	for _ in range(U):
 		x = np.random.choice(img.shape[1])
 		y = np.random.choice(img.shape[2])
-		perturbed = perturbed(img, x, y, p)
-		if label not in top_k_predicitons(model, [perturbed], 1):
+		perturb = perturbed(img, x, y, p)
+		if label not in top_k_predicitons(model, perturb.reshape(1, 3, 32, 32), 1):
 			critical += 1
 	return float(critical) / U
 
 
 def locsearchadv(model, img, p, r, d, t, k, R, label):
 	dim1, dim2 = image.shape[1], image.shape[2]
-	PX, PY = np.random.choice(range(int(dim1)),int(dim1*0.1)), np.random.choice(range(int(dim2)),int(dim2*0.1))
-	I = np.copy(img)
+	num_pixels = int(dim1*dim2*0.1)
+	PX, PY = np.random.choice(range(int(dim1)),num_pixels), np.random.choice(range(int(dim2)),num_pixels)
 	i = 1
 	while i <= R:
+		I = np.copy(img)
 		# Computing the function g using the neighborhood
 		L = []
 		for j in range(len(PX)):
 				L.append(perturbed(I, PX[j], PY[j], p))
 		L = np.array(L)
-		scores = -1 * score(model, L, label)
+		scores = model.predict_proba(L)[:,label]
 		sorted_L = np.argsort(scores)
 		PX = (PX[sorted_L])[:t]
 		PY = (PY[sorted_L])[:t]
@@ -62,21 +68,27 @@ def locsearchadv(model, img, p, r, d, t, k, R, label):
 		for j in range(len(PX)):
 			I = perturbed(I, PX[j], PY[j], r)	
 		# Check whether the perturbed image I is an adversarial image
-		if label not in top_k_predicitons(model, [perturbed], k):
-			return True
+		predictions = top_k_predicitons(model, I.reshape(1, 3, 32, 32), k)
+		if label not in predictions:
+			return (True, label)
 		# Update a neighborhood of pixel locations for the next round
 		PX_ , PY_ = [], []
 		for j in range(len(PX)):
 			for k in range(-d,d+1):
 				x_co = PX[j] + k
-				y_co = PY[j] + k
-				if x_co > 0 and x_co < I.shape[1] and y_co > 0 and y_co < Y.shape[2]:
-					PX_.append(x_co)
-					PY_.append(y_co)
+				if x_co >0 and x_co < I.shape[1]:
+					for l in range(-d,d+1):
+						y_co = PY[j] + l
+						if y_co > 0 and y_co < I.shape[2]:
+							PX_.append(x_co)
+							PY_.append(y_co)
+		show_image(I)
 		PX, PY = np.array(PX_), np.array(PY_)
 		i += 1
-	return False
+	return (False, -1)
 
 
 if __name__ == "__main__":
 	model = load_model(sys.argv[1])
+	image = np.load(sys.argv[2])
+	print(locsearchadv(model, image, p=1, r=1, d=3, t=10, k=2, R=4, label=0))
